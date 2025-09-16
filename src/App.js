23280a2f4200d873cac8aec933d71feedd4407c1
Luhcas28 <136550705+Luhcas28@users.noreply.github.com>
2025-09-15 19:14:35 -0500

    Configuración de App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import PokemonList from './components/pokemon-list/pokemon-list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Pokédex App - Especies</h1>
+          <p>Explora todas las especies de Pokémon</p>
+        </header>
+        <main>
+          <PokemonList />
+        </main>
+      </div>
   );
 }
 
